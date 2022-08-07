219d08075515e63cfc70387e3d492b995a6870aa
Felipe Lorenci <lorencifelipe@gmail.com>
2022-08-06 22:57:12 -0300

    Created Class problem.

---
+++ problem.py
@@ -0,0 +1,76 @@
+import pandas as pd
+from pulp import *
+
+# Class Problem
+class Problem:
+
+    # Indexer method -> Make pvt
+    def idx(self, a, b):
+        return(str(a)+"_"+str(b))
+
+    def idx(self, a, b, c):
+        return(str(a)+"_"+str(b)+"_"+str(c))
+
+    # Method to load file -> Make pvt
+    def loadFile(self, file):
+        return pd.read_csv(file, encoding="latin1")
+
+    # Method to load containeres -> Make pvt
+    def loadContainers(self):
+        return list(self.df["container"].unique())
+
+    # Method to load boxes -> Make pvt
+    def loadBoxes(self):
+        boxes = []
+        for a in self.indexed_boxes:
+            for b in self.indexed_boxes[a]:
+                boxes.append(self.idx(a, b))
+        return boxes
+
+    # Method to load indexed boxes -> Make pvt
+    def loadIndexedBoxes(self):
+        indexedBoxes = {}
+        for a in self.containeres:
+            indexedBoxes[a] = list(self.df.query(
+                "container == @a")["box"].unique())
+        return indexedBoxes
+
+    # Method to load indexed Cylinders -> Make pvt
+    def loadIndexedCylinders(self):
+        indexedCylinders = {}
+        for a in self.indexedBoxes:
+            for b in self.indexedBoxes[a]:
+                indexedCylinders[self.idx(a, b)] = list(
+                    self.df.query("container == @a and box == @b")["cylinder"].unique())
+        return indexedCylinders
+
+    # Method to load cylinders -> Make pvt
+    def loadCylinders(self):
+        cylinders = []
+        for b in self.indexedCylinders:
+            for c in self.indexedCylinders[b]:
+                cylinders.append(self.idx(b, c))
+        return cylinders
+
+    # Method to load weight/volume -> Make pvt
+    def loadInfo(self, column):
+        info = {}
+        for a in self.indexedBoxes:
+            for b in self.indexedBoxes[a]:
+                cylindersIndexes = list(self.df.query(
+                    "container == @a and box == @b")["cylinder"].unique())
+                for c in cylindersIndexes:
+                    info[self.idx(a, b, c)] = round(float(self.df.query(
+                        "container == @a and box == @b and cylinder == @c")[column]), 2)
+        return info
+
+    # Constructor
+    def __init__(self, file):
+        self.df = self.loadFile(file)
+        self.containers = self.loadContainers()
+        self.indexedBoxes = self.loadIndexedBoxes()
+        self.boxes = self.loadBoxes()
+        self.indexedCylinders = self.loadIndexedCylinders()
+        self.cylinders = self.loadCylinders()
+        self.cylindersWeight = self.loadInfo("cylinderWeight")
+        self.cylindersVolume = self.loadInfo("cylindersVolume")
--- raizen.py
+++ raizen.py
@@ -1,6 +1,77 @@
 import pandas as pd
 from pulp import *
+
+# PENDENCIAS
+# Arredondamento na leitura
+# indexer
+
+
+def checkSolution():
+    ok = True
+
+    # check number of containeres
+    numberX = 0
+    for a in containeres:
+        if value(x[a]) == 1:
+            numberX += 1
+    if numberX != 35:
+        ok = False
+
+    ## ARREDONDAMENTO
+    # # check volume
+    # totalVolume = 0
+    # for c in cylinders:
+    #     if value(z[c]) == 1:
+    #         totalVolume += cylinders_volume[c]
+    # if totalVolume != 5163.69:
+    #     ok = False
+
+    # check weight
+    totalWeight = 0
+    for c in cylinders:
+        if value(z[c]) == 1:
+            totalWeight += cylinders_weight[c]
+    if totalWeight != 18844:
+        ok = False
+
+    # Verificar se para cada container selecionado, há pelo menos uma caixa selecionada
+    # E para cada container NÃO selecionado, não podem haver caixas selecionadas
+    for a in containeres:
+        if value(x[a]) == 1:
+            for b in indexed_boxes[a]:
+                if value(y[a+"_"+b]) == 1:
+                    break
+            else:
+                ok = False
+        else:
+            for b in indexed_boxes[a]:
+                if value(y[a+"_"+b]) == 1:
+                    ok = False
+            # indexed_boxes
+
+    # Verificar se para cada caixa selecionada, há EXATAMENTE 1 cilindro DESSA CAIXA selecionado
+    # E para cada caixa não selecionada, não podem haver cilindros selecionados
+    for b in boxes:
+        if value(y[b]) == 1:
+            cylinderCounter = 0
+            for c in indexed_cylinders[b]:
+                if value(z[b+"_"+str(c)])==1:
+                    cylinderCounter+=1
+            if cylinderCounter != 1:
+                ok = False
+        else:
+            cylinderCounter = 0
+            for c in indexed_cylinders[b]:
+                if value(z[b+"_"+str(c)])==1:
+                    cylinderCounter+=1
+            if cylinderCounter > 0:
+                ok = False
+
+
+    return ok
+
 # Leitura
+
 
 file_name = "data.csv"
 
@@ -13,26 +84,27 @@ indexed_boxes = {}
 for a in containeres:
     indexed_boxes[a] = list(df.query("container == @a")["box"].unique())
 
-boxes = []
+boxes = [] 
 
 for a in indexed_boxes:
     for b in indexed_boxes[a]:
         boxes.append(a+"_"+b)
 
-indexed_cylinders = {}
+indexed_cylinders = {}  
+
 
 for a in indexed_boxes:
     for b in indexed_boxes[a]:
         indexed_cylinders[a+"_"+b] = list(
             df.query("container == @a and box == @b")["cylinder"].unique())
 
-cylinders = []
+cylinders = [] 
 
 for b in indexed_cylinders:
     for c in indexed_cylinders[b]:
         cylinders.append(b+"_"+str(c))
 
-cylinders_weight = {}
+cylinders_weight = {}  
 cylinders_volume = {}
 
 for a in indexed_boxes:
@@ -46,7 +118,7 @@ for a in indexed_boxes:
             cylinders_volume[a+"_"+b+"_"+str(c)] = float(
                 df.query("container == @a and box == @b and cylinder == @c")["cylinderVolume"])
 
-
+# STOP HERE
 # Modelo
 prob = LpProblem("Raizen")
 
@@ -77,22 +149,66 @@ prob += lpSum([cylinders_volume[c]*z[c] for c in cylinders]) == 5163.69
 prob += lpSum([cylinders_weight[c]*z[c] for c in cylinders]) == 18844
 
 for a in containeres:
-    prob += lpSum([y[a+"_"+b] for b in indexed_boxes[a]]) <= x[a]*len(indexed_boxes)
+    prob += lpSum([y[a+"_"+b] for b in indexed_boxes[a]]
+                  ) <= x[a]*len(indexed_boxes[a])  # update form
 
-for a in containeres: 
+for a in containeres:
     prob += lpSum([y[a+"_"+b] for b in indexed_boxes[a]]) >= x[a]
 
 for b in boxes:
     prob += y[b] == lpSum([z[b+"_"+str(c)] for c in indexed_cylinders[b]])
 
-#call solver
-prob.solve()
 
-for a in x:
-    print(a + ": " + str(value(x[a])))
+# Extra constraints
 
-for a in y:
-    print(a + ": " + str(value(y[a])))
+# Para cada container, se
 
-for a in z:
-    print(a + ": " + str(value(z[a])))
+solution_counter = 0
+
+
+def countZ(z):
+    c = 0
+    for a in z:
+        if(value(z[a]) == 1):
+            c += 1
+    return c
+
+
+while True:
+    prob.solve()
+    if LpStatus[prob.status] == "Optimal":
+        for a in x:
+            print(a + ": " + str(value(x[a])))
+
+        for a in y:
+            print(a + ": " + str(value(y[a])))
+
+        for a in z:
+            print(a + ": " + str(value(z[a])))
+
+        z_count = countZ(z)
+        # cut opt. sol.
+        #prob += lpSum([z[c] for c in cylinders if value(z[c]) == 1]) <= 34
+        prob += lpSum([z[c]
+                       for c in cylinders if value(z[c]) == 1]) <= z_count - 1
+
+        solution_counter += 1
+        print("Solution counter: " + str(solution_counter))
+    else:
+        break
+    ok = checkSolution()
+    print(ok)
+
+
+print("Total solutions: " + str(solution_counter))
+# #call solver
+# prob.solve()
+
+# for a in x:
+#     print(a + ": " + str(value(x[a])))
+
+# for a in y:
+#     print(a + ": " + str(value(y[a])))
+
+# for a in z:
+#     print(a + ": " + str(value(z[a])))
